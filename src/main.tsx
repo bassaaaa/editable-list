ada94fe8241f6c619b7fba4544fcfb4e2a85f5a4
Tsubasa Ito <t.ito@chuo-ad.co.jp>
2024-05-14 14:15:50 +0900

    EditStateProviderを追加

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import { App } from './App';
 import './css/globals.css';
 import { ItemListProvider } from './provider/ItemListProvider';
+import { EditStateProvider } from './provider/EditStateProvider';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ItemListProvider>
-      <App />
+      <EditStateProvider>
+        <App />
+      </EditStateProvider>
     </ItemListProvider>
   </React.StrictMode>
 );
