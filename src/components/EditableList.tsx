115cba3c5c77f9675365667b1a6462cd3466d9d1
bassaaaa <white.galaxys2.album2@gmail.com>
2024-05-15 21:25:24 +0900

    propsを修正

--- src/components/EditableList.tsx
+++ src/components/EditableList.tsx
@@ -1,22 +1,21 @@
 import { FC } from 'react';
 import { EditStateProvider } from '../provider/EditStateProvider';
 import { ItemListProvider } from '../provider/ItemListProvider';
-import { ItemList } from '../types';
 import { InputWithButton } from './InputWithButton';
 import { SortableList } from './SortableList';
+import { Item } from '../types';
 
 type Props = {
-  initialItemList: ItemList;
+  title: string;
+  items: Item[];
 };
 
-export const EditableList: FC<Props> = ({ initialItemList }) => {
+export const EditableList: FC<Props> = ({ title, items }) => {
   return (
-    <ItemListProvider initialItems={initialItemList.items}>
+    <ItemListProvider initialItems={items}>
       <EditStateProvider>
         <div className="w-full p-6 bg-base-200 rounded-md mx-auto">
-          <h1 className="text-2xl font-bold text-center mb-6">
-            {initialItemList.title}
-          </h1>
+          <h1 className="text-2xl font-bold text-center mb-6">{title}</h1>
           <InputWithButton placeholder="追加する項目を入力" />
           <SortableList />
         </div>
